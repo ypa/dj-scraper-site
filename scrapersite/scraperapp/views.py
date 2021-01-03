e9bba5f126c4c5f590c27d94a8928afd4211c401
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-02 19:51:36 -0800

    Add scrape view that scrapes a hard-coded url for now

--- scrapersite/scraperapp/views.py
+++ scrapersite/scraperapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
+
+def scrape(request):
+    requests.get('https://wwww.google.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    link_addresses = [link.get('href') for link in soup.find_all('a')]
+
+    return render(request, 'scraperapp/result.html',
+                  {'link_addresses': link_addresses})
+
